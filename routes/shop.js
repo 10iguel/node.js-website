4d86f17cfee6e55b3700d6b6b9f04fb1824ef2ef
Miguel <miguel.espinoza.silloca@gmail.com>
2020-11-21 23:32:39 -0500

    Updating my routes and controllers

--- routes/shop.js
+++ routes/shop.js
@@ -4,13 +4,15 @@ const express = require('express')
 
 const rootDir = require('../utils/path')
 const adminData = require('./admin')
-const {getIndex,getProducts,getCart,getCheckout,getOrders} = require('../controllers/shop')
+const {getIndex,getProducts,getProduct,getCart,getCheckout,getOrders} = require('../controllers/shop')
 
 const router = express.Router()
 
 router.get('/',getIndex)
 
 router.get('/products',getProducts)
+
+router.get('/products/productId',getProduct)
 
 router.get('/cart',getCart)
 
